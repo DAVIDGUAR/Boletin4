c3f1b69f482144402e900565d7be23a41f8d5f29
drodriguezguardiola <drodriguezguardiola@nautilus19.danielcastelao.org>
2018-10-30 10:20:34 +0100

    comentarios javadoc

--- boletin4/Boletin4_1/src/boletin4_1/Boletin4_1.java
+++ boletin4/Boletin4_1/src/boletin4_1/Boletin4_1.java
@@ -1,11 +1,26 @@
 
 package boletin4_1;
 
-
+/**
+ * modificar la velocidad vehiculo
+ * @author drodriguezguardiola
+ * @since octubre 2018
+ * @version 1.0
+ * 
+ */
 public class Boletin4_1 {
 
 
     public static void main(String[] args) {
+        
+        
+        /**
+         * @param crea el obx velocidadCoche
+         * @param incrementa la velocidad
+         * @param reduce la velocidad
+         * @param muestra la velocidad actual del vehiculo
+         * 
+         */
         
         Coche velocidadCoche = new Coche();
         velocidadCoche.acelerar(80);
